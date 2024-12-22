a4dbc7239f72a82d3dd1e406c3994abb3617f1ac
Adriano FRANCI <adriano.franci@spire.com>
2024-12-22 17:42:12 +0100

    fix : adjust colortheme

--- neovim_config/lua/plugins/colors.lua
+++ neovim_config/lua/plugins/colors.lua
@@ -1,7 +1,7 @@
 function SetColorScheme(color)
     vim.cmd.colorscheme(color)
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
@@ -14,39 +14,15 @@ return {
         config = function()
             require("catppuccin").setup({
                 flavour = "macchiato",
+                show_end_of_buffer = true,
+                styles = {
+                    strings = {'italic'},
+                    comments = {'altfont'}
+                    -- comments = {'bold','underdouble'}
+                }
             })
             SetColorScheme("catppuccin")
         end
-    },
-    {
-        "ellisonleao/gruvbox.nvim",
-        name = "gruvbox",
-        config = function()
-            require("gruvbox").setup({
-                terminal_colors = true, -- add neovim terminal colors
-                undercurl = true,
-                underline = false,
-                bold = true,
-                italic = {
-                    strings = false,
-                    emphasis = false,
-                    comments = false,
-                    operators = false,
-                    folds = false,
-                },
-                strikethrough = true,
-                invert_selection = false,
-                invert_signs = false,
-                invert_tabline = false,
-                invert_intend_guides = false,
-                inverse = true, -- invert background for search, diffs, statuslines and errors
-                contrast = "", -- can be "hard", "soft" or empty string
-                palette_overrides = {},
-                overrides = {},
-                dim_inactive = false,
-                transparent_mode = false,
-            })
-        end,
     },
     {
         "folke/tokyonight.nvim",
@@ -71,20 +47,7 @@ return {
         end
     },
 
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function()
-            require('rose-pine').setup({
-                disable_background = true,
-                styles = {
-                    italic = false,
-                },
-            })
 
-            -- SetColorScheme("rose-pine");
-        end
-    },
 
 
 }

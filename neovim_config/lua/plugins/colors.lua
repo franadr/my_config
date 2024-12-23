bb49172f410544246a64f686350a65f4bc3c09db
Adriano FRANCI <adriano.franci@spire.com>
2024-12-23 18:23:37 +0100

    lint : formating

--- neovim_config/lua/plugins/colors.lua
+++ neovim_config/lua/plugins/colors.lua
@@ -1,5 +1,3 @@
-
-
 return {
 
     {
@@ -12,8 +10,8 @@ return {
                 flavour = "macchiato",
                 show_end_of_buffer = true,
                 styles = {
-                    strings = {'italic'},
-                    comments = {'altfont'}
+                    strings = { 'italic' },
+                    comments = { 'altfont' }
                     -- comments = {'bold','underdouble'}
                 }
             })
@@ -25,8 +23,8 @@ return {
             require("tokyonight").setup({
                 -- your configuration comes here
                 -- or leave it empty to use the default settings
-                style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-                transparent = false, -- Enable this to disable setting the background color
+                style = "moon",         -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+                transparent = false,    -- Enable this to disable setting the background color
                 terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
                 styles = {
                     -- Style to be applied to different syntax groups
@@ -35,7 +33,7 @@ return {
                     keywords = { italic = false },
                     -- Background styles. Can be "dark", "transparent" or "normal"
                     sidebars = "dark", -- style for sidebars, see below
-                    floats = "dark", -- style for floating windows
+                    floats = "dark",   -- style for floating windows
                 },
             })
         end
@@ -45,4 +43,3 @@ return {
 
 
 }
-

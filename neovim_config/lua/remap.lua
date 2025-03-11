7f5fac213d9d167e56120a6d9c29b17765a3a7b9
Adriano FRANCI <adriano.franci@l-acoustics.com>
2025-03-11 15:37:54 +0100

    format + add ZAQ for closing vim

--- neovim_config/lua/remap.lua
+++ neovim_config/lua/remap.lua
@@ -3,7 +3,7 @@
 =========================INSPIRED KICKSTART REMAPS===================
 https://github.com/nvim-lua/kickstart.nvim?tab=readme-ov-file
 =====================================================================
-]]--
+]] --
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
@@ -27,11 +27,11 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 -- vim: ts=2 sts=2 sw=2 et
@@ -40,31 +40,35 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 =====================================================================
 =========================CUSTOM REMAPS===============================
 =====================================================================
-]]--
+]] --
 
 
 -- allow to move higlighted block up and down with J, K
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv",{desc = "Allow to move highlighted block (visual) up"})
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv",{desc = "Allow to move highlighted block (visual) down"})
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Allow to move highlighted block (visual) up" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Allow to move highlighted block (visual) down" })
 
 -- With J you can append the line bellow to the end of the current line without moving the cursor
-vim.keymap.set('n', 'J', "mzJ`z",{desc="Append the line bellow to the end of current line while keeping cursor position"})
+vim.keymap.set('n', 'J', "mzJ`z",
+    { desc = "Append the line bellow to the end of current line while keeping cursor position" })
 
 -- Remar C-d and C-u to move half page down/up without moving the cursor
-vim.keymap.set("n", "<C-d>", "<C-d>zz",{desc="move down 1/2 page without moving cursor"})
-vim.keymap.set("n", "<C-u>", "<C-u>zz",{desc="move up 1/2 page without moving cursor"})
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "move down 1/2 page without moving cursor" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "move up 1/2 page without moving cursor" })
 
 -- Allow to keep the search term in the middle of the page
-vim.keymap.set("n", "n", "nzzzv",{desc="keep search term in mid page"})
-vim.keymap.set("n", "N", "Nzzzv",{desc="keep search term in mid page"})
+vim.keymap.set("n", "n", "nzzzv", { desc = "keep search term in mid page" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "keep search term in mid page" })
 
 -- Allow to paste with <leader>p without replacing the register value for the subsequent pastes(keeping the initialy copied value)
-vim.keymap.set("x", "<leader>p", [["_dP]],{desc="Paste without replacing reg value for the subsequent paste"})
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste without replacing reg value for the subsequent paste" })
 
 -- Set <leader>y to yank to the system register (system clipboard)
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]],{desc="yank to system reg"})
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "yank to system reg" })
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 -- <leader>s Will spawn a command to replace the word selected
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],{desc="replace selected word"})
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+    { desc = "replace selected word" })
 
+-- Quit vim completely and save ZQA
+vim.keymap.set("n", "ZAQ", ":wqa <CR>", { desc = "Close vim and save" })
